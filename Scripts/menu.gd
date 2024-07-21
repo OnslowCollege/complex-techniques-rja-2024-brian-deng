178e0d8a9b1f5a772388f87f13d81036fea0b9e4
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-07-21 23:55:17 +1200

    Setup scenes for menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/StartButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +16,12 @@ func _on_start_button_pressed():
 
 
 func _on_option_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/options.tscn")
 
 
 func _on_tutorial_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/tutorial.tscn")
 
 
 func _on_quit_button_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
