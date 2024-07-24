d0fe879d8f5e0b83afc209ebb0af8ac56c3c7fc5
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-07-24 13:21:42 +1200

    Learning GD script

--- Main.gd
+++ Main.gd
@@ -1,9 +1,9 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Hello World"
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scenes/tutorial.gd
+++ Scenes/tutorial.gd
@@ -1,9 +1,9 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Hello World"
+	$VBoxContainer.alignment
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
