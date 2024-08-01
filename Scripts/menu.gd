2707b92fba514c5085deee8797cf25a764b52fc1
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-08-02 11:17:39 +1200

    more learning gdscript

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -9,8 +9,28 @@ func _ready():
 	title.modulate = Color.GHOST_WHITE
 	title.horizontal_alignment = 1
 	title.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	add_child(title)
 
+	add_child(title)
+	$title.text = "Poker Game"
+	$title.size.x = 100
+	$title.add_theme_font_size_override("Poker Game", 1000)
+	# Assuming you have a label node called "my_label"
+	var label = $title
+
+	# Create a new DynamicFont
+	var new_font = FontFile.new()
+
+	# Load a font file (.ttf, .otf, etc.)
+	var font_file = load("res://Assets/vcr_osd_mono/VCR_OSD_MONO_1.001.ttf") as FontFile
+	
+	new_font.font_data = font_file
+
+	# Set the desired font size
+	new_font.size = 24
+
+	# Assign the new font to the label
+	label.add_theme_font("font", new_font)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
