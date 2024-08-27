5eeb57d1daec3b0ef7c51d538918cf736ed28985
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-08-27 15:10:19 +1200

    remove label and opacity

--- Scenes/more_tutorial.gd
+++ Scenes/more_tutorial.gd
@@ -10,15 +10,18 @@ func _process(delta):
 	pass
 
 # prevent errors for more button presses
-var button_presses = 0
+var button_presses = 1
+# label defined here so can be altered properly
+var handtext: Label = null
+
 func _on_next_pressed():
-	var handtext = Label.new()
-	if button_presses == 0:
+	if button_presses == 1:
 		# removes previous labels
 		$Community.queue_free()
 		$Hand2.queue_free()
 		
 		# creates new label on first button press
+		handtext = Label.new()
 		handtext.set_name("Hand3")
 		handtext.text = "This specific hand can be matched with the 5 community
 			cards. In particular this hand gives 9 pair and 10 high.
@@ -26,18 +29,25 @@ func _on_next_pressed():
 			card in the community cards which is the 3. Then seeing if
 			your 10 can be swapped which it can with the 8."
 		handtext.add_theme_font_size_override("font_size",20)
-
+		
+		# Changing opacity of cards
+		$"river/4_spades".modulate.a = 0.5
+		$"river/8_spades".modulate.a = 0.5
+		
 		# positioning of label
 		$".".add_child(handtext)
 		var window_x = get_viewport().size[0]
 		var window_y = get_viewport().size[1]
 		var handtext_length = handtext.get_rect().size[0]
-		print(handtext_length)
 		handtext.position = Vector2((window_x/2-handtext_length/2+50),(window_y/2+120))
-		
-		
-		# Makes so that only first button press does anything
+			# Makes so that only first button press does the label
 		button_presses += 1
-	if button_presses == 1:
-		handtext = 0
-	
+		
+	elif button_presses == 2:
+		# Removes the label on the second button press
+		if handtext != null:
+			handtext.queue_free()
+			handtext = null  # Optional: reset the reference to null
+		button_presses += 1
+		#get_tree().quit()
+
