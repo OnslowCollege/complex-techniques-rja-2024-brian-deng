98283ec122dad8c27378684b4d0b00b244f09c0f
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-08-27 13:20:43 +1200

    More label

--- Scenes/more_tutorial.gd
+++ Scenes/more_tutorial.gd
@@ -12,30 +12,32 @@ func _process(delta):
 # prevent errors for more button presses
 var button_presses = 0
 func _on_next_pressed():
+	var handtext = Label.new()
 	if button_presses == 0:
 		# removes previous labels
 		$Community.queue_free()
 		$Hand2.queue_free()
 		
 		# creates new label on first button press
-		var handtext = Label.new()
 		handtext.set_name("Hand3")
 		handtext.text = "This specific hand can be matched with the 5 community
 			cards. In particular this hand gives 9 pair and 10 high.
 			This is done by swapping the 9 in your hand for the lowest
 			card in the community cards which is the 3. Then seeing if
 			your 10 can be swapped which it can with the 8."
-		handtext.add_theme_font_size_override("",20)
+		handtext.add_theme_font_size_override("font_size",20)
 
 		# positioning of label
+		$".".add_child(handtext)
 		var window_x = get_viewport().size[0]
 		var window_y = get_viewport().size[1]
-		var handtext_length = handtext.get_rect().size
+		var handtext_length = handtext.get_rect().size[0]
 		print(handtext_length)
-		handtext.position = Vector2((window_x/2),(window_y/2+100))
-		$".".add_child(handtext)
+		handtext.position = Vector2((window_x/2-handtext_length/2+50),(window_y/2+120))
+		
 		
 		# Makes so that only first button press does anything
 		button_presses += 1
-	
+	if button_presses == 1:
+		handtext = 0
 	
