80dd4bd20bcba778778c3d3e908cbdd8ed63ce56
BrianD6500 <100812193+BrianD6500@users.noreply.github.com>
2024-08-28 21:39:26 +1200

    action tutorial finished

--- Scripts/action_tutorial.gd
+++ Scripts/action_tutorial.gd
@@ -1,107 +1,103 @@
 extends Control
 
-# make node for arrow at start and then function to change properties 
-
-func arrow(start_point: Vector2, end_point: Vector2, color: Color, width: float):
-	# Create the Line2D for the arrow shaft
-	var arrow_line = Line2D.new()
-	arrow_line.width = width
-	arrow_line.default_color = color
-	
-	# Points for the shaft (start and end of the arrow)
-	arrow_line.add_point(start_point)
-	arrow_line.add_point(end_point)
-	
-	# Add the Line2D to the scene
-	$".".add_child(arrow_line)
-	# Create the Polygon2D for the arrowhead
-	var arrowhead = Polygon2D.new()
-	
-	# Calculate direction and base of the arrowhead
-	var direction = (end_point - start_point).normalized()
-	var perpendicular = Vector2(-direction.y, direction.x) * (width * 2)
-	var arrowhead_base_left = end_point + perpendicular
-	var arrowhead_base_right = end_point - perpendicular 
-	
-	# Define the points of the arrowhead (triangle)
-	var points = PackedVector2Array()
-	points.append(end_point + direction * (width * 4)) # Tip of the arrow
-	points.append(arrowhead_base_left)    # Left base
-	points.append(arrowhead_base_right)   # Right base
-	arrowhead.polygon = points
-	arrowhead.color = color  # Set arrowhead color
-	# Add the Polygon2D to the scene
-	$".".add_child(arrowhead)
-
+# Creates arrow for proper explanation of the tutorial message/ action buttons
+# 
+# Parameters:
+#  - start (Vector2): The starting position of the arrow.
+#  - end (Vector2): The ending position of the arrow, where the arrowhead will be placed.
+#  - width (float): The width of the arrow's line, and the size basis for the arrowhead.
+# 
 func arrow_change(start: Vector2, end: Vector2, width: float):
+	# Making arrow base or the line.
 	$arrow/Line.clear_points()
 	$arrow/Line.add_point(start)
 	$arrow/Line.add_point(end)
 	$arrow/Line.width = width
 	
+	# Setting vars for proper arrow head orientation and size
 	var direction = (end - start).normalized()
 	var perpendicular = Vector2(-direction.y, direction.x) * (width * 2)
 	var arrowhead_base_left = end + perpendicular
 	var arrowhead_base_right = end - perpendicular 
 	
+	# Making polygon/ arrow head
 	var points = PackedVector2Array()
 	points.append(end + direction * (width * 4)) # Tip of the arrow
-	points.append(arrowhead_base_left) #+ Vector2(-100, -100))    # Left base
+	points.append(arrowhead_base_left) # Left base
 	points.append(arrowhead_base_right)   # Right base
-	$arrow/head.polygon = points
-	$arrow/head.position = Vector2(0,0)
-
-	print(start, end)
-	print(points)
-	print(arrowhead_base_left)
 	
+	# Setting polygon
+	$arrow/head.polygon = points
+	# To ensure the polygon positioned correctly
+	$arrow/head.position = Vector2(0,0) 
 
-var fold_arrow: Line2D = null
 # prevent errors for more button presses
 var button_presses = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	arrow_change(Vector2(1000,500), Vector2(1100, 700), 10)
-	#fold_arrow = arrow($Fold_text.position + (Vector2(200, 60)), 
-	#$Button_bg.position + (Vector2(150, -30)), Color.WHITE, 9)
-	#if button_presses == 2:
-		#fold_arrow.queue_free()
-		#fold_arrow = null
+	arrow_change($Fold_text.position + (Vector2(200, 60)), 
+		$Button_bg.position - Vector2(-170, 30), 10)
 	
 # label defined here so can be altered properly
-var handtext: Label = null
+var tutorial_text: Label = null
 
 func _on_next_button_pressed():
 	if button_presses == 1:
 		# removes previous labels
-		$Fold_text.queue_free()
-		#.queue_free()
+		$Fold_text.visible = false
 		
 		# creates new label on first button press
-		handtext = Label.new()
-		handtext.set_name("Hand3")
-		handtext.text = "This specific hand can be matched with the 5 community
-			cards. In particular this hand gives 9 pair and 10 high.
-			This is done by swapping the 9 in your hand for the lowest
-			card in the community cards which is the 3. Then seeing if
-			your 10 can be swapped which it can with the 8."
-		handtext.add_theme_font_size_override("font_size",20)
+		tutorial_text = Label.new()
+		tutorial_text.set_name("Hand3")
+		tutorial_text.text = "
+		Bet means to bet an amount of money into 
+		the pot(money you can win). When betting 
+		or raising(when someone else has bet and 
+		then you bet a higher amount) the other 
+		players have to call to continue playing 
+		for the pot."
+
+		tutorial_text.add_theme_font_size_override("font_size",20)
 		
 		# positioning of label
-		$".".add_child(handtext)
+		$".".add_child(tutorial_text)
 		var window_x = get_viewport().size[0]
 		var window_y = get_viewport().size[1]
-		var handtext_length = handtext.get_rect().size[0]
-		handtext.position = Vector2((window_x/2-handtext_length/2+50),(window_y/2+120))
-			# Makes so that only first button press does the label
-		button_presses += 1
+		var tutorial_text_length = tutorial_text.get_rect().size[0]
+		tutorial_text.position = Vector2(650,640)
 		
+		# Arrow
+		arrow_change($Fold_text.position + (Vector2(200, 80)), 
+			$Button_bg.position - Vector2(-390, 50), 10)
+		
+		# Makes so that only first button press does the label
+		button_presses += 1
+
+	# Tutorial text for the check button
 	elif button_presses == 2:
-		# Removes the label on the second button press
-		if handtext != null:
-			handtext.queue_free()
-			handtext = null
+		tutorial_text.text = "
+		Check is when no one has bet so you also
+		choose to not bet."
+		tutorial_text.position = Vector2(650,715)
+		
+		# Arrow
+		arrow_change($Fold_text.position + (Vector2(300, 80)), 
+			$Button_bg.position - Vector2(-600, 50), 10)
+
+		button_presses += 1
+
+	# Tutorial text for the call button
+	elif button_presses == 3:
+		tutorial_text.text = "
+		Call is when someone has bet an amount and
+		you bet the same amount
+		"
+		tutorial_text.position = Vector2(640,715)
+		
+		# Arrow
+		arrow_change($Fold_text.position + (Vector2(400, 80)), 
+			$Button_bg.position - Vector2(-800, 50), 10)
 		button_presses += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
